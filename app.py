d16d1b0a552930958d7a86dcf67c9ba429b75a3c
miku <svartkatt39@gmail.com>
2023-03-24 08:21:16 +0100

    add error handling

--- app.py
+++ app.py
@@ -34,35 +34,38 @@ def index():
         END_DATE=request.form["end_date"]
         # print(START_DATE)
 
-        cred = get_cred()
-        posts = get_posts(CHANNEL, START_DATE, END_DATE)
+        try:
+            cred = get_cred()
+            posts = get_posts(CHANNEL, START_DATE, END_DATE)
+
+            service = discovery.build('sheets', 'v4', credentials=cred)
 
 
-        service = discovery.build('sheets', 'v4', credentials=cred)
+            RANGE_NAME = 'slackapp_test!A2' #シートと書き込み開始位置を指定/定値
 
-
-        RANGE_NAME = 'slackapp_test!A2' #シートと書き込み開始位置を指定/定値
-
-        value_input_option = 'RAW'
-        range_name = RANGE_NAME
-        values = posts
-        data = [
-            {
-                'range': range_name,
-                'values': values
-            },
-        ]
-        body = {
-        'valueInputOption': value_input_option,
-        'data': data
-        }
-        result = service.spreadsheets().values().batchUpdate(
-            spreadsheetId=SPREADSHEET_ID, body=body).execute()
-        print(result)
-        return redirect(url_for('slack', mtd=method))
+            value_input_option = 'RAW'
+            range_name = RANGE_NAME
+            values = posts
+            data = [
+                {
+                    'range': range_name,
+                    'values': values
+                },
+            ]
+            body = {
+            'valueInputOption': value_input_option,
+            'data': data
+            }
+            result = service.spreadsheets().values().batchUpdate(
+                spreadsheetId=SPREADSHEET_ID, body=body).execute()
+            print(result)
+            return redirect(url_for('slack', mtd=method))
+        except:
+            return render_template("index.html", method="error")
     else:
         method="get"
         return redirect(url_for('slack', mtd=method))
+
     
 
 if __name__ == "__main__":
